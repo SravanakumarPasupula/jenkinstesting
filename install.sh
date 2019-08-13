357113367aec8a3ff766e8970c7c9d2360e47764
sravanakumar pasupula <sravanakumar.pasupula@gmail.com>
2019-08-14 03:17:25 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 sudo apt-get install tree
-rm -rf /tmp/folder3
+rm -rf /tmp/folder
 mkdir /tmp/folder
+echo "------------------------------------------------------"
 touch /tmp/folder/testfile1
-touch /tmp/folder/testfile2
-touch /tmp/folder/testfile3
+echo "------------------------------------------------------"
+echo echo $(date) >> /tmp/folder/testfile1
+sleep 3s
+echo "wait maadukoli appa "
+echo "------------------------------------------------------"
+echo "------------------------------------------------------"
+echo "------------------------------------------------------"
+echo echo $(date) >> /tmp/folder/testfile1
+sleep 3s
+echo "wait maadukoli appa "
+echo "------------------------------------------------------"
+echo "------------------------------------------------------"
+echo "------------------------------------------------------"
+echo echo $(date) >> /tmp/folder/testfile1
+sleep 3s
+echo "wait maadukoli appa "
+echo "------------------------------------------------------"
+echo "------------------------------------------------------"
+echo "------------------------------------------------------"
+
+echo echo $(date) >> /tmp/folder/testfile1
+sleep 3s
+echo "wait maadukoli appa "
+echo "------------------------------------------------------"
+echo "------------------------------------------------------"
+echo "------------------------------------------------------"
 tree /tmp/folder
 
 
