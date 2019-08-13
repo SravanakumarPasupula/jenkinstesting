efbc216ea7d17f9389b7b08519404919aedb9696
sravanakumar pasupula <sravanakumar.pasupula@gmail.com>
2019-08-14 02:50:25 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 sudo apt-get update
-rm -rf /tmp/folder1
-rm -rf /tmp/folder
-
-mkdir /tmp/folder2
-touch /tmp/folder2/testfile1
-touch /tmp/folder2/testfile2
-touch /tmp/folder2/testfile3
+rm -rf /tmp/folder2
+mkdir /tmp/folder3
+touch /tmp/folder3/testfile1
+touch /tmp/folder3/testfile2
+touch /tmp/folder3/testfile3
 
